18d580dca881c93ca0e387394f04625c3fe6b1a2
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2018-03-01 23:26:12 -0300

    testing

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully! Changed!",
+        "message": "Go Serverless v1.0! Your function executed successfully! Changed again!",
         "input": event
     }
 
